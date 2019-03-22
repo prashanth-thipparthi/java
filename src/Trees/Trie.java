c8a873de7addbd722d379fdbb86baea6f3e739b6
prashanth.thipparthi <prashanth.reddyt09@gmail.com>
2019-03-21 22:26:08 -0600

    code change

--- src/Trees/Trie.java
+++ src/Trees/Trie.java
@@ -39,6 +39,34 @@ public class Trie {
         current.isEnd = true;
     }
 
+    /*
+    recursive insert
+     */
+
+    public void insertRecursive(String word){
+        int index = 0;
+        insertRecursive(word,root,index);
+    }
+
+    public boolean insertRecursive(String word, TrieNode current,int index){
+        if(index == word.length()){
+            current.isEnd = true;
+            return true;
+        }
+
+        char ch = word.charAt(index);
+        TrieNode node = current.children.get(ch);
+
+        if(node==null){
+            node = new TrieNode();
+            current.children.put(ch,node);
+        }
+
+        insertRecursive(word,node,index+1);
+        return false;
+    }
+
+
     public boolean search(String word){
 
         TrieNode current = root;
@@ -53,6 +81,58 @@ public class Trie {
         return current.isEnd;
     }
 
+    /*
+    recursive implementation of search
+     */
+
+    public void recursiveSearch(String word){
+        int index = 0;
+        recursiveSearch(word,root,index);
+    }
+
+    public boolean recursiveSearch(String word, TrieNode current, int index){
+
+        if(index == word.length()){
+            return current.isEnd;
+        }
+
+        char ch = word.charAt(index);
+        TrieNode node = current.children.get(ch);
+        if(node == null){
+            return false;
+        }
+        return recursiveSearch(word,node,index+1);
+    }
+
+    public boolean deleteWord(String word){
+        int index = 0;
+        return delete(word,root,index);
+    }
+
+    public boolean delete(String word,TrieNode current,int index){
+        if(index == word.length()){
+            if(!current.isEnd){
+                return false;
+            }
+
+            current.isEnd = false;
+            return current.children.size() == 0;
+        }
+        char ch = word.charAt(index);
+        TrieNode node = current.children.get(ch);
+        if(node==null){
+            return false;
+        }
+
+        boolean curentNodeDeleteCondition = delete(word,node,index+1);
+
+        if(curentNodeDeleteCondition){
+            current.children.remove(ch);
+            return current.children.size() == 0;
+        }
+        return false;
+    }
+
     public static void main(String args[]){
         Trie t = new Trie();
         String keys[] = {"the", "a", "there", "answer", "any",
@@ -60,10 +140,15 @@ public class Trie {
 
         String output[] = {"Not present in trie", "Present in trie"};
         for(int i=0;i<keys.length;i++){
-            t.insert(keys[i]);
+            //t.insert(keys[i]);
+            t.insertRecursive(keys[i]);
         }
 
-        System.out.println("Word Found:"+t.search("b"));
+        System.out.println("Word Found:"+t.search("there"));
+
+        System.out.println("String Delete Condition:"+t.deleteWord("there"));
+
+        System.out.println("Word Found:"+t.search("there"));
     }
 
 }
